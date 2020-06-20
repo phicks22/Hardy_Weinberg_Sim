6ac42ef6fdfeb518ff0150313b826d11a2b5fdf7
phicks22 <65268541+phicks22@users.noreply.github.com>
2020-06-20 15:43:37 -0700

    Added frequency function to Population class

--- Model/Population.py
+++ Model/Population.py
@@ -1,6 +1,12 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 class Population:
 
     def __init__(self, p, w11, w12, w22, n, gen, u, v, inf_pop):
+        
         """ Population class for setting a population to calculate object attributes after
          the number of generations given.
 
@@ -25,3 +31,21 @@ class Population:
         self.u = u
         self.v = v
         self.inf_pop = inf_pop
+
+    def fitness(self):
+
+        """Calculates the frequency of p after selection.
+
+        Args:
+            None
+
+        Returns:
+            self.p
+
+        """
+        w_bar = (self.w11 * math.pow(self.p, 2)) + (self.w12 * (2 * self.p * (1 - self.p))) + \
+                (self.w22 * math.pow((1 - self.p), 2))
+        p_t = ((math.pow(self.p, 2) * self.w11) + (self.p * (1 - self.p) * self.w12) / w_bar)
+        self.p = p_t
+
+        return self.p

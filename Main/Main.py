1752b1855fdfb1efe81c1eb339ba74a297039e52
phicks22 <65268541+phicks22@users.noreply.github.com>
2020-06-21 22:10:19 -0700

    Temporarily removed dictionary and implemented plot function

--- Main/Main.py
+++ Main/Main.py
@@ -1,9 +1,6 @@
 from Model.Population import Population
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
-import pandas as pd
-from collections import defaultdict
 
 
 class Main(Population):
@@ -18,7 +15,7 @@ class Main(Population):
     def __init__(self, p, w11, w12, w22, gen, n, u, v, m, inf_pop, num_pop):
         super().__init__(p, w11, w12, w22, gen, n, u, v, m, inf_pop)
         self.num_pop = num_pop
-        self.pop_dict = {}
+        self.pop_dict = dict()
 
     def calculate_p_t(self):
         """Calculates p over t generations implementing fitness, mutation, migration, and drift.
@@ -32,42 +29,51 @@ class Main(Population):
 
         """
         pop_list = []
-
+        pop_freq = []
         for i in range(self.num_pop):
             pop_list.append(Population)
 
             # self.pop_dict = defaultdict(list)
 
-        for item in pop_list:
-            self.pop_dict = defaultdict(item)
-            # self.pop_dict[Main].append(item)
+        # for item in pop_list:
+        #     self.pop_dict[item] = pop_freq
+        #     # self.pop_dict[Main].append(item)
 
-        for key in self.pop_dict:
+        # for key in self.pop_dict:
+        # for i in pop_list:
             for generation in range(self.gen):
-                pop_p = []
 
-                for population in self.pop_dict:
+                for population in pop_list:
                     population.fitness(self)
                     population.mutation(self)
                     population.migration(self)
                     population.genetic_drift(self)
-                    pop_p.append(self.p)
+                    pop_freq.append(self.p)
 
                 # self.plot(pop_p)
                 # return print(pop_p)
-            return self.pop_dict[key].append(pop_list)
+            # self.pop_dict[key].append(pop_list)
 
-        return print(self.pop_dict)
+        return self.plot(pop_freq)
 
-    def plot(self, pop_p):
+    def plot(self, pop_freq):
+        """Plots frequency of p over x generations for each population.
+
+        Args:
+            pop_freq
+
+        Returns:
+            None
+
+        """
         t = np.arange(0.0, self.gen, 1)
-        s = pop_p
+        s = pop_freq
 
         fig, ax = plt.subplots()
         ax.plot(t, s)
 
-        ax.set(xlabel='Generations', ylabel='Frequency',
-               title='p')
+        ax.set(xlim=(0, self.gen), ylim=(0, 1), xlabel='Generations', ylabel='Frequency',
+               title='Frequency p')
         ax.grid()
 
         plt.show()
@@ -75,7 +81,7 @@ class Main(Population):
 # pop = Main(p, w11, w12, w22, gen, n, u, v, m, inf_pop, num_pop)
 
 
-pop = Main(0.9, 1.0, 1.0, 1.0, 100, 100, 0.0, 0.0, 0.0, False, 3)
+pop = Main(0.2, 0.7, 1.0, 1.0, 1000, 100, 0.0, 0.0, 0.0, False, 1)
 
 
 pop.calculate_p_t()
--- Model/Population.py
+++ Model/Population.py
@@ -1,7 +1,5 @@
 import math
-import matplotlib.pyplot as plt
 import numpy as np
-import random
 
 
 class Population:
